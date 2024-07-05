7ca64ecc83879998b2ef93a6d6585a6d5cd240f7
sheilaokwisa544 <159282123+sheilaokwisa544@users.noreply.github.com>
2024-07-05 15:32:12 +0300

    Add files via upload

---
+++ assignment2.py
@@ -0,0 +1,12 @@
+# write a program that prompts:
+# a user to enter a number checks
+# whether a number is prime or odd
+
+# prime numbers
+y = 0
+if y %
+
+# a program that allows you to enter an alphabet as  either
+# as a vowel or a constant
+
+# write your own program to demonstrate the concept of inheritance in python
